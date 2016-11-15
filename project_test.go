109daad17cae51ce334f4b62507799aa12458b82
Nancy HO <ltho@clustertech.com>
2016-11-15 17:47:01 +0800

    add API to retrieve project by ID

--- project_test.go
+++ project_test.go
@@ -77,3 +77,48 @@ func TestProjectListWithCompleteness(t *testing.T) {
 		t.Error("Expected no error, got ", err)
 	}
 }
+
+func TestProjectGet(t *testing.T) {
+	accountid := os.Getenv("ACE_ACCOUNTID")
+	username := os.Getenv("ACE_USERNAME")
+	password := os.Getenv("ACE_PASSWORD")
+
+	if accountid == "" || username == "" || password == "" {
+		t.Error("ACE_ACCOUNTID, ACE_USERNAME, ACE_PASSWORD are not set in environment variable")
+	}
+
+	authInfo := aceproject.AuthInfo{accountid, username, password}
+	client := &http.Client{}
+	svc := aceproject.NewLoginService(client)
+	guidInfo, _, err := svc.Login(&authInfo)
+
+	if guidInfo == nil {
+		t.Error("Expected to login success")
+	}
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+
+	proj := aceproject.NewProjectService(client, guidInfo)
+
+	projects, _, err := proj.List()
+
+	if projects == nil {
+		t.Error("Expected to have a project list, but it is nil")
+	} else if len(projects) == 0 {
+		t.Error("Expected to have a project list, but size=", len(projects))
+	}
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+
+	projectID := projects[0].ID
+	project, _, err := proj.Get(projectID)
+
+	if project == nil {
+		t.Error("Expected to get project, but it is nil")
+	}
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+}
