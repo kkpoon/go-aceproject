c77f41260fd5965c2b8f0cba669c56790f2804a5
Nancy HO <nancyho@clustertech.com>
2018-03-19 10:04:45 +0800

    Add function for listing time sheets by user and date range (#15)

--- timesheet_test.go
+++ timesheet_test.go
@@ -91,3 +91,40 @@ func TestDailyTimesheetListWithDateRange(t *testing.T) {
 		}
 	}
 }
+
+func TestDailyTimesheetListWithUserIDDateRange(t *testing.T) {
+	accountid := os.Getenv("ACE_ACCOUNTID")
+	username := os.Getenv("ACE_USERNAME")
+	password := os.Getenv("ACE_PASSWORD")
+
+	if accountid == "" || username == "" || password == "" {
+		t.Error("ACE_ACCOUNTID, ACE_USERNAME, ACE_PASSWORD are not set in environment variable")
+	}
+
+	authInfo := aceproject.AuthInfo{accountid, username, password}
+	client := &http.Client{}
+	svc := aceproject.NewLoginService(client)
+	guidInfo, _, err := svc.Login(&authInfo)
+
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+	if guidInfo == nil {
+		t.Error("Expected to login success")
+	} else {
+		tsSvc := aceproject.NewTimesheetService(client, guidInfo)
+
+		ts, _, err := tsSvc.ListAllDailyWithUserIDDateRange(
+			int64(1),
+			time.Date(2016, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2016, 10, 6, 0, 0, 0, 0, time.UTC),
+		)
+
+		if ts == nil {
+			t.Error("Expected to have a daily time sheet list, but it is nil")
+		}
+		if err != nil {
+			t.Error("Expected no error, got ", err)
+		}
+	}
+}
