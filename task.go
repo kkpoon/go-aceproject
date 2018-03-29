7217551da0c1add1c1803cad39ef8e7607ebcf29
Nancy HO <nancyho@clustertech.com>
2018-03-29 09:03:00 +0800

    Add more fields to task and allow to get tasks by last update date (#18)

--- task.go
+++ task.go
@@ -2,13 +2,17 @@ package aceproject
 
 import (
 	"net/http"
+	"time"
 
 	sling "gopkg.in/dghubble/sling.v1"
 )
 
 // GetTasksParam represents gettasks request parameter
 type GetTasksParam struct {
-	ProjectID int64 `url:"ProjectID,omitempty"`
+	ProjectID               *int64  `url:"ProjectID,omitempty"`
+	FilterFirstDate         *string `url:"FilterFirstDate,omitempty"`
+	FilterFirstDateOperator *int    `url:"FilterFirstDateOperator,omitempty"`
+	FilterFirstDateValue    *string `url:"FilterFirstDateValue,omitempty"`
 }
 
 // TaskResponse represents task listing response
@@ -19,13 +23,21 @@ type TaskResponse struct {
 
 // Task is representing task in ACEProject
 type Task struct {
-	ID            int64   `json:"TASK_ID"`
-	Name          string  `json:"TASK_RESUME"`
-	TaskGroupID   int64   `json:"TASK_GROUP_ID"`
-	TaskGroupName string  `json:"TASK_GROUP_NAME"`
-	ProjectID     int64   `json:"PROJECT_ID"`
-	ProjectName   string  `json:"PROJECT_NAME"`
-	ErrorDesc     *string `json:"ERRORDESCRIPTION,omitempty"`
+	ID               int64   `json:"TASK_ID"`
+	Name             string  `json:"TASK_RESUME"`
+	TaskGroupID      int64   `json:"TASK_GROUP_ID"`
+	TaskGroupName    string  `json:"TASK_GROUP_NAME"`
+	TaskStatusName   string  `json:"TASK_STATUS_NAME"`
+	ProjectID        int64   `json:"PROJECT_ID"`
+	ProjectName      string  `json:"PROJECT_NAME"`
+	DateTaskCreated  string  `json:"DATE_TASK_CREATED"`
+	DateTaskModified string  `json:"DATE_TASK_MODIFIED"`
+	UserCreatorID    int64   `json:"USER_CREATOR_ID"`
+	Username         string  `json:"USERNAME"`
+	UpdateUserID     int64   `json:"UPDATE_USER_ID"`
+	UpdateUsername   string  `json:"UPDATE_USERNAME"`
+	Assigned         string  `json:"ASSIGNED"`
+	ErrorDesc        *string `json:"ERRORDESCRIPTION,omitempty"`
 }
 
 // TaskService provides methods to interact with task specific action
@@ -60,7 +72,30 @@ func (s *TaskService) ListWithProject(projectID int64) ([]Task, *http.Response, 
 	resObj := new(TaskResponse)
 	resp, err := s.sling.New().
 		QueryStruct(CreateFunctionParam("gettasks")).
-		QueryStruct(&GetTasksParam{ProjectID: projectID}).
+		QueryStruct(&GetTasksParam{ProjectID: &projectID}).
+		ReceiveSuccess(resObj)
+	if resObj != nil && len(resObj.Results) > 0 {
+		if resObj.Results[0].ErrorDesc != nil {
+			return nil, resp, Error{*resObj.Results[0].ErrorDesc}
+		}
+		return *(&resObj.Results), resp, err
+	}
+	return make([]Task, 0), resp, err
+}
+
+// ListWithLastModifiedDate returns all tasks modified on or after the specified date
+func (s *TaskService) ListWithLastModifiedDate(modifiedDate time.Time) ([]Task, *http.Response, error) {
+	filterDateType := "DATE_TASK_MODIFIED"
+	filterDateOperator := 3 // Is Later Than Or On
+	modifiedDateStr := modifiedDate.Format("2006-01-02")
+	resObj := new(TaskResponse)
+	resp, err := s.sling.New().
+		QueryStruct(CreateFunctionParam("gettasks")).
+		QueryStruct(&GetTasksParam{
+			FilterFirstDate:         &filterDateType,
+			FilterFirstDateOperator: &filterDateOperator,
+			FilterFirstDateValue:    &modifiedDateStr,
+		}).
 		ReceiveSuccess(resObj)
 	if resObj != nil && len(resObj.Results) > 0 {
 		if resObj.Results[0].ErrorDesc != nil {
--- task_test.go
+++ task_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"testing"
+	"time"
 
 	aceproject "github.com/kkpoon/go-aceproject"
 )
@@ -80,6 +81,41 @@ func TestTaskListWithProjectId(t *testing.T) {
 
 		projectID := projects[0].ID
 		tasks, _, err := taskSvc.ListWithProject(projectID)
+
+		if tasks == nil {
+			t.Error("Expected to have a task list, but it is nil")
+		} else if len(tasks) == 0 {
+			t.Error("Expected to have a task list, but size=", len(tasks))
+		}
+		if err != nil {
+			t.Error("Expected no error, got ", err)
+		}
+	}
+}
+
+func TestTaskListWithLastModifiedDate(t *testing.T) {
+	accountid := os.Getenv("ACE_ACCOUNTID")
+	username := os.Getenv("ACE_USERNAME")
+	password := os.Getenv("ACE_PASSWORD")
+
+	if accountid == "" || username == "" || password == "" {
+		t.Error("ACE_ACCOUNTID, ACE_USERNAME, ACE_PASSWORD are not set in environment variable")
+	}
+
+	authInfo := aceproject.AuthInfo{accountid, username, password}
+	client := &http.Client{}
+	svc := aceproject.NewLoginService(client)
+	guidInfo, _, err := svc.Login(&authInfo)
+
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+	if guidInfo == nil {
+		t.Error("Expected to login success")
+	} else {
+		taskSvc := aceproject.NewTaskService(client, guidInfo)
+
+		tasks, _, err := taskSvc.ListWithLastModifiedDate(time.Now())
 
 		if tasks == nil {
 			t.Error("Expected to have a task list, but it is nil")
